dc4d9b589bef275169ed08c79eac31cfd40a5890
Egor Parh <133140409+Egorlog13432@users.noreply.github.com>
2024-09-18 10:02:44 +0300

    rust by pact 1-4

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -1,14 +1,38 @@
-
-#[test]
-fn test() {
-    assert_eq!(2 + 3, 5);
-}
-
 #[test]
 fn test1() {
-    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let x: i32 = 5;
     let _y: i32;
 
     assert_eq!(x, 5);
     println!("Success!");
+}
+
+#[test]
+fn test2() {
+    let mut x: i32 = 1;
+    x += 2;
+
+    assert_eq!(x, 3);
+    println!("Success!");
+}
+#[test]
+// ?????????
+fn test3() {
+    let x: i32 = 10;
+    let y: i32 = 5;
+    {
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y);
+}
+
+#[test]
+fn test4() {
+    let x = define_x();
+    println!("{}, world", x);
+}
+
+fn define_x() -> String{
+    let x = "hello";
+    x.to_string()
 }

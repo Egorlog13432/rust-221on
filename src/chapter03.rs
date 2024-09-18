00e7696d9ddf22ddcd7dcd2c12929d739dd9f510
Egor Parh <133140409+Egorlog13432@users.noreply.github.com>
2024-09-18 09:30:44 +0300

    first

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -7,7 +7,7 @@ fn test() {
 #[test]
 fn test1() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
-    let _y: i32; // Uninitialized but also unused, only a Warning !
+    let _y: i32;
 
     assert_eq!(x, 5);
     println!("Success!");

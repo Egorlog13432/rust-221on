776d92db3c293f11ffe4e6f0298674670f70a532
Egor Parh <133140409+Egorlog13432@users.noreply.github.com>
2024-09-25 09:23:24 +0300

    rust by pact 1-4

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -36,3 +36,57 @@ fn define_x() -> String{
     let x = "hello";
     x.to_string()
 }
+
+#[test]
+fn test5() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
+
+    assert_eq!(x, 5);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
+
+#[test]
+fn test6() {
+    let mut x: i32 = 1;
+    x = 7;
+    // Shadowing and re-binding
+    let mut x = x;
+    x += 3;
+
+
+    let y = 4;
+    // Shadowing
+    let y = "I can also be bound to text!";
+
+    println!("Success!");
+}
+
+#[test]
+fn test7() {
+    let x = 1;
+    println!("{}", x);
+}
+
+#[test]
+fn test7_1() {
+    let x = 1;
+    let y = x + 2; // Use x in a calculation
+    println!("{}", y); // This will print 3
+}
+
+#[test]
+fn test8() {
+    let (x, y);
+    (x,..) = (3, 4);
+    [.., y] = [1, 2];
+    // Fill the blank to make the code work
+    assert_eq!([x,y], [3, 2]);
+
+    println!("Success!");
+}

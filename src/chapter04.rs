a23f974148307a08a69f2db2a27465b86f6814fd
Egor Parh <133140409+Egorlog13432@users.noreply.github.com>
2024-09-26 10:10:33 +0300

    rust by pact 4.1 8 - 11

--- src/chapter04.rs
+++ src/chapter04.rs
@@ -1,3 +1,5 @@
+use std::ops::{Range, RangeInclusive};
+
 #[test]
 fn test1() {
     let x: i32 = 5;
@@ -69,15 +71,57 @@ fn type_of2<T>(_: &T) -> String {
 
 #[test]
 fn test8() {
-    let result = (0.1 + 0.2 * 10.0).round() / 10.0; // Round to one decimal place
-    let expected = 0.3;
-
-    assert!(result == expected); // This should now work as expected
+    assert!(0.1 as f32+0.2 as f32==0.3 as f32);
 
     println!("Success!");
 }
 
 #[test]
 fn test9() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
 
+    assert!(sum == -5);
+
+    for c in 'a'..='z' {
+        println!("{}",c as u8);
+    }
+}
+
+#[test]
+fn test10() {
+    assert_eq!((1..5), Range{ start: 1, end: 5 });
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+
+    println!("Success!");
+}
+
+#[test]
+fn test11() {
+    // Integer addition
+    assert!(1u32 + 2u32 == 3u32);
+
+    // Integer subtraction
+    assert!(1i32 - 2i32 == -1i32);
+    assert!(1i8 - 2i8 == -1i8);
+
+    assert!(3 * 50 == 150);
+
+    assert!(9.6 as f32 / 3.2 as f32 == 3.0 as f32); // error ! make it work
+
+    assert!(24 % 5 == 4);
+
+    // Short-circuiting boolean logic
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 }

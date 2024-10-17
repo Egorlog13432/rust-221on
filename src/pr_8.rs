2fa28998fbf2d674bc4c84583bbcc1eb5bf9a95c
Egor Parh <133140409+Egorlog13432@users.noreply.github.com>
2024-10-17 21:53:59 +0300

    pact 8

---
+++ src/pr_8.rs
@@ -0,0 +1,36 @@
+#[test]
+fn test() {
+    let data =
+        [
+            ("Hello", "hELLO"),
+            ("Привет", "пРИВЕТ"),
+        ];
+
+
+    data
+        .iter()
+        .for_each(|(a, b)| {
+            assert_eq!(
+                invert_the_case(a.to_string()),
+                b.to_string()
+            );
+            assert_eq!(
+                invert_the_case(b.to_string()),
+                a.to_string()
+            );
+        });
+}
+
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_ascii_uppercase() {
+                c.to_ascii_lowercase()
+            } else if c.is_ascii_lowercase() {
+                c.to_ascii_uppercase()
+            } else {
+                c
+            }
+        })
+        .collect()
+}
